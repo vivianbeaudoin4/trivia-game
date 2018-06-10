7d971475d2cd4881a141b6cf0054517a2c98e4c0
vivianbeaudoin4 <vivianj481@gmail.com>
2018-06-09 20:57:48 -0400

    final scoring

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-score=0;
-question=1;
+var score=0;
+var question=1;
+var index=0;
+var answers=["0", "octopus", "daffodil", "cats", "12", "1971", "force", "Abraham Lincoln", "Alexandrina", "red blood cell"];
 
 $(document).ready(main);
 function main()
@@ -11,6 +13,7 @@ function main()
 function select()
 {
     var clickID;
+    var correct=answers[index];
     clickID=$(this).attr("id");
     if(clickID=="correct")
     {
@@ -24,6 +27,9 @@ function select()
         $("#correctAns").hide();
     }
     $(scoreNum).text("Score: "+score);
+    $(answer).text("The correct answer was "+correct);
+    $("#answer").show();
+    index++;
     change();
 }
 
@@ -38,6 +44,35 @@ function change()
         $("#again").show();
         $("#wrongAns").hide();
         $("#correctAns").hide();
+        $("#answer").hide();
+        $("#scoreNum").hide();
+        $(scoreDiv).text(score+"/50");
+        $("#scoreDiv").show();
+        if(score>40)
+        {
+            $(rank).text("That's awesome dude!");
+            $("#rank").show();
+        }
+        else if(score>30)
+        {
+            $(rank).text("Not too horrible!");
+            $("#rank").show();
+        }
+        else if(score>20)
+        {
+            $(rank).text("You're pretty average.");
+            $("#rank").show();
+        }
+        else if(score>10)
+        {
+            $(rank).text("You could do better.");
+            $("#rank").show();
+        }
+        else
+        {
+            $(rank).text("Did you even read the questions?");
+            $("#rank").show();
+        }
     }
 }
 
@@ -46,6 +81,9 @@ function hideQuestions()
     $("#wrongAns").hide();
     $("#correctAns").hide();
     $("#again").hide();
+    $("#scoreDiv").hide();
+    $("#rank").hide();
+    $("#answer").hide();
     $("#done").hide();
     $("#q2").hide();
     $("#q3").hide();
